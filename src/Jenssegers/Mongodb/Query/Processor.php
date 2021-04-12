ffb2c4e2b0e6dc3dfcddcd7fa6c3ed585251b037
Marshall Miller <contact@broken-titan.com>
2021-04-12 14:03:31 -0400

    Adds transaction support.

--- src/Jenssegers/Mongodb/Query/Processor.php
+++ src/Jenssegers/Mongodb/Query/Processor.php
@@ -6,4 +6,17 @@ use Illuminate\Database\Query\Processors\Processor as BaseProcessor;
 
 class Processor extends BaseProcessor
 {
+	/**
+     * Process an  "insert get ID" query.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  string  $sql
+     * @param  array  $values
+     * @param  string|null  $sequence
+     * @return mixed
+     */
+    public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
+    {
+        return $query->getConnection()->insert($values);
+    }
 }
