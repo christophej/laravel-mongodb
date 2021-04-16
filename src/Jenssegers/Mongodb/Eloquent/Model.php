b231b16922d3f09e1ba4d6b9c33baf1e36110e8c
Marshall Miller <contact@marshall-miller.com>
2021-04-16 02:08:42 -0400

    Update Model.php

--- src/Jenssegers/Mongodb/Eloquent/Model.php
+++ src/Jenssegers/Mongodb/Eloquent/Model.php
@@ -44,29 +44,6 @@ abstract class Model extends BaseModel
     protected $parentRelation;
 
     /**
-     * Custom accessor for the model's id.
-     * @param mixed $value
-     * @return mixed
-     */
-    public function getIdAttribute($value = null)
-    {
-        // If we don't have a value for 'id', we will use the Mongo '_id' value.
-        // This allows us to work with models in a more sql-like way.
-        if (!$value && array_key_exists('_id', $this->attributes)) {
-            $value = $this->attributes['_id'];
-        }
-
-        // Convert ObjectID to string.
-        if ($value instanceof ObjectID) {
-            return (string) $value;
-        } elseif ($value instanceof Binary) {
-            return (string) $value->getData();
-        }
-
-        return $value;
-    }
-
-    /**
      * @inheritdoc
      */
     public function getQualifiedKeyName()
