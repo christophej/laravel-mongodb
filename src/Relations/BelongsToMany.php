a93cd9d2b8f4078b0a82ed1656f2c174665b7fc4
Marshall Miller <contact@marshall-miller.com>
2021-11-03 03:04:42 -0400

    Update BelongsToMany.php
    Force array keys to be sequential.

--- src/Relations/BelongsToMany.php
+++ src/Relations/BelongsToMany.php
@@ -243,7 +243,7 @@ class BelongsToMany extends EloquentBelongsToMany
         // If associated IDs were passed to the method we will only delete those
         // associations, otherwise all of the association ties will be broken.
         // We'll return the numbers of affected rows when we do the deletes.
-        $ids = (array) $ids;
+        $ids = array_values((array) $ids);
 
         // Detach all ids from the parent model.
         $this->parent->pull($this->getRelatedKey(), $ids);
