c9e39123efdcb0cfa231095e3b10fbb491bde13c
Marshall Miller <contact@marshall-miller.com>
2023-07-26 16:13:48 -0400

    Added batch queue support.

--- src/MongodbServiceProvider.php
+++ src/MongodbServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Jenssegers\Mongodb;
 
+use Illuminate\Bus\{BatchFactory, DatabaseBatchRepository};
 use Illuminate\Support\ServiceProvider;
+use Jenssegers\Mongodb\Bus\MongoDatabaseBatchRepository;
 use Jenssegers\Mongodb\Eloquent\Model;
 use Jenssegers\Mongodb\Queue\MongoConnector;
 
@@ -37,6 +39,14 @@ class MongodbServiceProvider extends ServiceProvider
             $queue->addConnector('mongodb', function () {
                 return new MongoConnector($this->app['db']);
             });
+        });
+
+        $this->app->extend(DatabaseBatchRepository::class, function ($command, $app) {
+            return new MongoDatabaseBatchRepository(
+                $app->make(BatchFactory::class),
+                $app->make('db')->connection($app->config->get('queue.batching.database')),
+                $app->config->get('queue.batching.table', 'job_batches')
+            );
         });
     }
 }
